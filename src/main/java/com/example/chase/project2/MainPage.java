06feeda161994c09822fdeaffe5ed75f467f23b5
lelouch <chasecullen@gmail.com>
2018-02-12 20:55:38 -0800

    3rd commit
    fixed photo quality issues while taking picture
    and corrected imageview scaling

--- src/main/java/com/example/chase/project2/MainPage.java
+++ src/main/java/com/example/chase/project2/MainPage.java
@@ -1,14 +1,20 @@
 package com.example.chase.project2;
 
+import android.content.ContentValues;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.view.View;
 import android.widget.Button;
 
@@ -24,6 +30,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.StringTokenizer;
 
 import java.io.FileNotFoundException;
@@ -45,11 +56,14 @@ import com.darwindeveloper.horizontalscrollmenulibrary.extras.MenuItem;
 
 public class MainPage extends AppCompatActivity {
     HorizontalScrollMenuView menu;
-    TextView textTargetUri;
     ImageView targetImage;
+    private String mCurrentPhotoPath;
     private Bitmap bmp;
     private Bitmap operation;
     private Bitmap newPicture;
+    private static final int LOAD_PICTURE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+    private static final int REQUEST_TAKE_PHOTO = 3;
     /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,13 +85,6 @@ public class MainPage extends AppCompatActivity {
 
         targetImage.setImageBitmap(default_bmp);
 
-
-
-
-
-
-
-
         initMenu();
 
 
@@ -85,9 +92,10 @@ public class MainPage extends AppCompatActivity {
 
             @Override
             public void onClick(View arg0) {
-                Intent intent = new Intent(Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, 0);
+               Intent intent = new Intent();
+               intent.setType("image/*");
+               intent.setAction(Intent.ACTION_GET_CONTENT);
+               startActivityForResult(Intent.createChooser(intent, "Select picture"), LOAD_PICTURE);
             }});
 
 
@@ -95,8 +103,7 @@ public class MainPage extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(photoCaptureIntent, 20);
+               dispatchTakePictureIntent();
             }});
 
         restoreButton.setOnClickListener(new View.OnClickListener(){
@@ -120,6 +127,109 @@ public class MainPage extends AppCompatActivity {
             newPicture = bitmap;
 
         }
+        if (requestCode == LOAD_PICTURE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap temp = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                targetImage.setImageBitmap(temp);
+                newPicture = temp;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            //Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+            //targetImage.setImageBitmap(bitmap);
+
+            //Bundle extras = data.getExtras();
+            //Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //targetImage.setImageBitmap(imageBitmap);
+
+            setPic();
+
+        }
+
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = targetImage.getWidth();
+        int targetH = targetImage.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        //bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        bitmap = RotateBitmap(bitmap, 90);
+        newPicture = bitmap;
+        targetImage.setImageBitmap(bitmap);
+    }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 
     private void initMenu() {
@@ -154,38 +264,13 @@ public class MainPage extends AppCompatActivity {
                         gama(targetImage);
                         break;
                     case "Gray":
-                        gray(targetImage);
+                        //gray(targetImage);
                         break;
                 }
             }
         });
 
 
-    }
-
-    public void gray(View view) {
-        BitmapDrawable abmp = (BitmapDrawable) targetImage.getDrawable();
-        bmp = abmp.getBitmap();
-
-        operation = Bitmap.createBitmap(bmp.getWidth(),bmp.getHeight(), bmp.getConfig());
-        double red = 0.33;
-        double green = 0.59;
-        double blue = 0.11;
-
-        for (int i = 0; i < bmp.getWidth(); i++) {
-            for (int j = 0; j < bmp.getHeight(); j++) {
-                int p = bmp.getPixel(i, j);
-                int r = Color.red(p);
-                int g = Color.green(p);
-                int b = Color.blue(p);
-
-                r = (int) red * r;
-                g = (int) green * g;
-                b = (int) blue * b;
-                operation.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
-            }
-        }
-        targetImage.setImageBitmap(operation);
     }
 
     public void blue(View view){
@@ -198,15 +283,16 @@ public class MainPage extends AppCompatActivity {
         for(int i=0; i<bmp.getWidth(); i++){
             for(int j=0; j<bmp.getHeight(); j++){
                 int p = bmp.getPixel(i, j);
-                int r = Color.red(p);
-                int g = Color.green(p);
+                Color.red(p);
+                int r;
+                Color.green(p);
+                int g;
                 int b = Color.blue(p);
-                int alpha = Color.alpha(p);
+                Color.alpha(p);
 
                 r =  0;
                 g =  0;
                 b =  b+150;
-                alpha = 0;
                 operation.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
             }
         }
@@ -223,15 +309,16 @@ public class MainPage extends AppCompatActivity {
         for(int i=0; i < bmp.getWidth(); i++){
             for(int j=0; j<bmp.getHeight(); j++){
                 int p = bmp.getPixel(i, j);
-                int r = Color.red(p);
+                Color.red(p);
+                int r;
                 int g = Color.green(p);
-                int b = Color.blue(p);
-                int alpha = Color.alpha(p);
+                Color.blue(p);
+                int b;
+                Color.alpha(p);
 
                 r =  0;
                 g =  g+150;
                 b =  0;
-                alpha = 0;
                 operation.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
             }
         }
@@ -250,14 +337,15 @@ public class MainPage extends AppCompatActivity {
             for(int j=0; j<bmp.getHeight(); j++){
                 int p = bmp.getPixel(i, j);
                 int r = Color.red(p);
-                int g = Color.green(p);
-                int b = Color.blue(p);
-                int alpha = Color.alpha(p);
+                Color.green(p);
+                int g;
+                Color.blue(p);
+                int b;
+                Color.alpha(p);
 
                 r =  r + 150;
                 g =  0;
                 b =  0;
-                alpha = 0;
                 operation.setPixel(i, j, Color.argb(Color.alpha(p), r, g, b));
             }
         }
